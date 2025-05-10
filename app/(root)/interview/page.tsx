c691694d8b7d5b545b0c201d0f03c184674e96c2
MD KAMRAN KHAN <kamrankaspersky@gmail.com>
2025-05-10 17:46:41 +0530

    show generated interviews on the homepage

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+// import React from 'react';
 import Agent from "@/components/Agent";
- const Page = () => {
+import {getCurrentUser} from "@/lib/actions/auth.action";
+ const Page = async () => {
+
+    const user = await getCurrentUser();
     return (
         <>
         <h3>Interview Generation</h3>
